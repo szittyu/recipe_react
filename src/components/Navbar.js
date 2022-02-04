eef8b1823c67e878f660693031fe98cb21937e7f
szittyu <szittyu1@gmail.com>
2022-02-04 16:57:18 +0100

    theme changer and mode done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,5 @@
 import {Link} from "react-router-dom"
-import { useContext } from "react"
-import { ThemeContext } from "../context/ThemeContext"
+import { useTheme } from "../hooks/useTheme"
 
 //styles
 import "./Navbar.css"
@@ -8,7 +7,7 @@ import "./Navbar.css"
 import Searchbar from "./Searchbar"
 
 export default function Navbar() {
-  const { color } = useContext(ThemeContext)
+  const { color } = useTheme()
 
   return (
     <div className="navbar" style={{ background: color}}>
